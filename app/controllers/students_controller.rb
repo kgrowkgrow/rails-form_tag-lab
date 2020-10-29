d3bd806291aaf46d48c5d5e963f76614179d93f6
kgrowkgrow <kevingrow11@gmail.com>
2020-10-29 15:45:19 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,23 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    render new_student_path
+  end
+
+  private 
+
+  def find_student
+    @student = Student.find(params[:id])
+  end
+
+  # def student_params
+  #   params.require(:student).permit(:first_name, :last_name)
+  # end
+
 end
